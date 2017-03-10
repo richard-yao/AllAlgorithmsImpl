28e7c8a0b771e957cb66304f0530489742725c82
richard-yao <yaoyxsgf@gmail.com>
2017-03-10 09:34:33 +0800

    keep maven project directory complete

---
+++ study-java/src/test/java/com/richard/test/maintest/MainTest.java
@@ -0,0 +1,13 @@
+package com.richard.test.maintest;
+/**
+* @author RichardYao richardyao@tvunetworks.com
+* @date Mar 10, 2017 9:31:32 AM
+*/
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
