d806184fdcf4b9f328c20bb65fba3016495436bf
richard-yao <yaoyxsgf@gmail.com>
2017-03-10 11:42:43 +0800

    use annotation and try to understand spring application context register beans

--- study-java/src/main/java/com/richard/test/spring/annotation/AnnotationUse.java
+++ study-java/src/main/java/com/richard/test/spring/annotation/AnnotationUse.java
@@ -7,24 +7,27 @@ import org.springframework.context.annotation.Configuration;
 * @author RichardYao richardyao@tvunetworks.com
 * @date Mar 10, 2017 10:41:15 AM
 * 使用自定义组件，这里必须把AnnotationUse加上Configuration注解，否则会导致Spring扫描InjectClass时找不到内部类定义
+* 或者不使用注解而单独在applicationContext容器中将AnnotationUse, InjectClass都注册了
 */
 @Configuration
 public class AnnotationUse {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-		applicationContext.register(AnnotationUse.class);
+		applicationContext.register(AnnotationUse.class); 
+		//applicationContext.register(InjectClass.class);
+		applicationContext.register(SimpleTest.class); //需要单独在applicationContext.register(class)中注册后才可以正常使用applicationContext.getBean(class)来实例化
 		applicationContext.refresh();
 		InjectClass injectClass = applicationContext.getBean(InjectClass.class);
 		injectClass.print();
-		SimpleTest beanTest = applicationContext.getBean(SimpleTest.class); //这里会抛出异常：No qualifying bean of type [com.richard.test.spring.annotation.SimpleTest] is defined
+		SimpleTest beanTest = applicationContext.getBean(SimpleTest.class);
 		beanTest.printOut();
 		applicationContext.close();
 	}
 	
 	/**
 	 * @author RichardYao
-	 * 这里必须是静态类才能使得applicationContext.getBean()方法调用正常, why?
+	 * applicationContext.getBean(Class ...)无法实例化非静态内部类
 	 */
 	@MyComponent
 	public static class InjectClass {
