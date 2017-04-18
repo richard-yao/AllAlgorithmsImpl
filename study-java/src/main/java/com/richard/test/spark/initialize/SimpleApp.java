8c5c826d230a6139892e39ab53ba4f7253efc887
richard-yao <yaoyxsgf@gmail.com>
2017-04-18 18:39:00 +0800

    use spark flatMap to deal collect data

--- study-java/src/main/java/com/richard/test/spark/initialize/SimpleApp.java
+++ study-java/src/main/java/com/richard/test/spark/initialize/SimpleApp.java
@@ -1,15 +1,23 @@
 package com.richard.test.spark.initialize;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.util.LongAccumulator;
+
+import scala.Tuple2;
 
 /**
 * @author RichardYao richardyao@tvunetworks.com
@@ -19,8 +27,14 @@ public class SimpleApp {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		String logFile = "hdfs://hadoop-master:9000/user/hadoop/input/test.log";
-		String masterAddress = "spark://hadoop-master:7077";
+		String logFile, masterAddress;
+		if(args != null && args.length == 2) {
+			logFile = args[0];
+			masterAddress = args[1];
+		} else {
+			logFile = "hdfs://hadoop-master:9000/user/hadoop/input/test.log";
+			masterAddress = "spark://hadoop-master:7077";
+		}
 		SparkConf conf = new SparkConf().setAppName("Simple-application").setMaster(masterAddress);
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> logData = sc.textFile(logFile).cache();
@@ -98,7 +112,62 @@ public class SimpleApp {
 		});
 		System.out.println("This text words' number is " + wordNumber + " and myself count value is "+totalWordNumber.value());
 		
+		useMapReduce(logData);
 		sc.stop();
+	}
+	
+	public static void useMapReduce(JavaRDD<String> logData) {
+		if(logData != null) {
+			
+			JavaPairRDD<String, Integer> pairCollect = logData.flatMapToPair(new PairFlatMapFunction<String, String, Integer>() {
+
+				private static final long serialVersionUID = 2843331512319295449L;
+
+				@Override
+				public Iterator<Tuple2<String, Integer>> call(String line) throws Exception {
+					String[] words = line.split("\t");
+					List<Tuple2<String, Integer>> result = new ArrayList<Tuple2<String, Integer>>();
+					for(int i=0;i<words.length;i++) {
+						Tuple2<String, Integer> temp = new Tuple2<String, Integer>(words[i], 1);
+						result.add(temp);
+					}
+					return result.iterator();
+				}
+			});
+			
+			/*JavaPairRDD<String, Integer> pairs = logData.mapToPair(new PairFunction<String, String, Integer>() {
+
+				private static final long serialVersionUID = -5217153855000840482L;
+
+				@Override
+				public Tuple2<String, Integer> call(String input) throws Exception {
+					Tuple2<String, Integer> addOne = new Tuple2<String, Integer>(input, 1);
+					return addOne;
+				}
+			});*/
+			JavaPairRDD<String, Integer> counts = pairCollect.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+				private static final long serialVersionUID = 6650917574451071587L;
+
+				@Override
+				public Integer call(Integer count1, Integer count2) throws Exception {
+					return count1 + count2;
+				}
+			});
+			counts.sortByKey();
+			List<Tuple2<String, Integer>> collectResult = counts.collect();
+			System.out.println("---------------Use collec() method start---------------");
+			for(int i=0;i<collectResult.size();i++) {
+				System.out.println("The key is "+collectResult.get(i)._1()+" and the value is "+collectResult.get(i)._2());
+			}
+			System.out.println("---------------Use collec() method end---------------");
+			System.out.println("---------------Use collectAsMap() method start---------------");
+			Map<String, Integer> result = counts.collectAsMap();
+			for(String key:result.keySet()) {
+				System.out.println("The key is " + key + " and the value is " + result.get(key));
+			}
+			System.out.println("---------------Use collectAsMap() method end---------------");
+		}
 	}
 
 }
